24482eed375d62eae2be7ad2393005bc2c7fba98
Lim <3269989624@qq.com>
2024-10-25 19:07:58 +0800

    24.10.25 :tada:

--- module03/string.js
+++ module03/string.js
@@ -8,8 +8,10 @@
 //方法
 //1.charAt(index )  //获取指定位置的字符
 //2.indexOf(string ) //获取所检索字符所在位置
-//3.trim()
-//4.subString(startIndex , endIndex(exclusive) )
+//3.trim()  //去除首尾空白
+//4.subString(startIndex , endIndex(exclusive) ) //截取字符串
+//5.endWith(string )   //是否以指定字符串结尾
+//6.7 toUpperCase() ,toLowerCase();  //大小写转化
 var str = " a bc  ";
 var strTrim = str.trim();
 console.log(str.length);
@@ -18,3 +20,4 @@ console.log(str.indexOf('a'));
 console.log(strTrim);
 console.log(str.substring(1,4));
 
+str.toLocaleUpperCase()
